243632e00003b138c0d563c858f5149901ddbbcd
Jiri Pesek <jiri.pesek@deutsche-boerse.com>
2024-04-28 21:32:39 +0200

    add godotenv config file package

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,17 +1,24 @@
 package main
 
 import (
+	"log"
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
 	router := chi.NewMux()
+
 	router.Get("/foo", handlerFoo)
 
-	listnerAddr := ":3000"
+	listnerAddr := os.Getenv("LISTEN_ADDR")
 	slog.Info("HTTP server started", "listnerAddr", listnerAddr)
 	http.ListenAndServe(listnerAddr, router)
 }
